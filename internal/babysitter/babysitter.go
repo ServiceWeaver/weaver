ServiceWeaver/weaver

commit 7e4ea73bac1a52ff413ee64301339112c3d1e882

Improved ExportListener API.
Before this PR, the flow for exporting a listener was a little
complicated. A weavelet would receive two listener related options via a
protos.Weavelet: use_localhost and process_picks_ports. If
use_localhost was true, a weavelet would listen on localhost.
Otherwise, it would listen on $HOSTNAME. If process_picks_ports was
true, the weavelet would listen on port 0. Otherwise, it would call
ExportListener to get a port. Thus, ExportListener sometimes exported a
listener and sometimes didn't (and returned a port).

This PR simplifies the API by splitting removing use_localhost and
process_picks_ports options completeley and by splitting ExportListener
in two. First, a weavelet calls GetAddress to get the address it should
listen on for a listener.  For example, the single process deployer may
return "localhost:0", while the SSH deployer may return "$HOSTNAME:0".
Second, the weavelet listens on this address and calls ExportListener to
export the listener.

This flow is much simpler, but it does require an additional RPC over
the pipe. However, exporting listeners is not on critical path, plus
there is already significant traffic over the pipe (for every log entry,
to export metrics, etc.). Overall, I think the performance hit is
negligible.

internal/babysitter/babysitter.go
@@ -297,8 +297,13 @@ func (b *Babysitter) ReportLoad(*protos.WeaveletLoadReport) error {
 	return nil
 }
 
+// GetAddress implements the protos.EnvelopeHandler interface.
+func (b *Babysitter) GetAddress(req *protos.GetAddressRequest) (*protos.GetAddressReply, error) {
+	return &protos.GetAddressReply{Address: "localhost:0"}, nil
+}
+
 // ExportListener implements the protos.EnvelopeHandler interface.
-func (b *Babysitter) ExportListener(req *protos.ListenerToExport) (*protos.ExportListenerReply, error) {
+func (b *Babysitter) ExportListener(req *protos.ExportListenerRequest) (*protos.ExportListenerReply, error) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
@@ -325,7 +330,8 @@ func (b *Babysitter) ExportListener(req *protos.ListenerToExport) (*protos.Expor
 
 	lis, err := net.Listen("tcp", req.LocalAddress)
 	if errors.Is(err, syscall.EADDRINUSE) {
-		return &protos.ExportListenerReply{AlreadyInUse: true}, nil
+		// Don't retry if this address is already in use.
+		return &protos.ExportListenerReply{Error: err.Error()}, nil
 	}
 	if err != nil {
 		return nil, fmt.Errorf("proxy listen: %w", err)
@@ -389,8 +395,6 @@ func (b *Babysitter) startProcess(dep *protos.Deployment, group *protos.Colocati
 			SameProcess:       dep.App.SameProcess,
 			Sections:          dep.App.Sections,
 			SingleProcess:     dep.SingleProcess,
-			UseLocalhost:      dep.UseLocalhost,
-			ProcessPicksPorts: dep.ProcessPicksPorts,
 			NetworkStorageDir: dep.NetworkStorageDir,
 		}
 		e, err := envelope.NewEnvelope(wlet, dep.App, b, b.opts)

runtime/protos/runtime.proto
@@ -47,11 +47,14 @@ message EnvelopeMsg {
   // A reply to the weavelet with the latest list of components to start.
   ComponentsToStart components_to_start = 7;
 
-  // A reply to the weavelet with the exported listener information.
-  ExportListenerReply export_listener_reply = 8;
+  // A reply to a GetAddressRequest.
+  GetAddressReply get_address_reply = 8;
+
+  // A reply to an ExportListenerRequest.
+  ExportListenerReply export_listener_reply = 9;
 
   // A reply to the weavelet with an error status.
-  string error = 9;
+  string error = 10;
 }
 
 // WeaveletMsg holds a message sent by a weavelet to the envelope that manages it.
@@ -98,8 +101,11 @@ message WeaveletMsg {
   // Request to get the list of components to be started.
   GetComponentsToStart get_components_to_start = 13;
 
+  // Request to get the address for a listener.
+  GetAddressRequest get_address_request = 14;
+
   // Request to export a listener.
-  ListenerToExport listener_to_export = 14;
+  ExportListenerRequest export_listener_request = 15;
 }
 
 // Deployment holds internal information necessary for an application
@@ -112,16 +118,6 @@ message Deployment {
 
   // Run as a single process locally.
   bool single_process = 10;
-
-  // If true, all network traffic is sent via localhost. This option should only
-  // be set if all weavelets are running on the same machine (e.g., when using
-  // the weaver-gke-local or weaver-multi deployers).
-  bool use_localhost = 9;
-
-  // If true, the process is expected to pick the first available port on the
-  // local machine when creating a new listener; otherwise, the process should
-  // consult the runtime to pick the listener ports.
-  bool process_picks_ports = 5;
 
   // Storage directory for files that facilitate internal communication
   // between Service Weaver processes. If empty, defaults to "$TMP_DIR/serviceweaver/network".
@@ -348,32 +344,46 @@ message ComponentsToStart {
   repeated string components = 3;
 }
 
-// ListenerToExport is a request to the envelope to export a process's network
-// listener.
-message ListenerToExport {
-  string app = 1;            // Service Weaver application
-  string deployment_id = 2;  // Service Weaver deployment id
-  string process = 3;        // name of the process whose weavelet exports listener
-
-  Listener listener = 5;      // listener specification.
-  string local_address = 6;   // LocalAddress argument in ListenerOptions
-  ColocationGroup group = 7;  // colocation group exporting the listener
+// GetAddressRequest is a request for the address a weavelet should listen on
+// for a particular listener.
+//
+// Imagine a weavelet calls GetListener with LocalAdress localhost:9000. Here
+// are some examples of how different deployers may handle a GetAddressRequest.
+//
+// - The singleprocess deployer may instruct the weavelet to listen directly
+//   on localhost:9000.
+// - The multiprocess deployer may instruct the weavelet to listen on
+//   localhost:0. It will separately start a proxy on localhost:9000.
+// - The SSH deployer may instruct the weavelet to listen on
+//   $HOSTNAME:0. It will separately start a proxy on localhost:9000.
+message GetAddressRequest {
+  string name = 1;           // listener name
+  string local_address = 2;  // LocalAddress argument in ListenerOptions
 }
 
-// ExportListenerReply is a result of the ListenerToExport request.
+// GetAddressReply is a reply to a GetAddressRequest.
+message GetAddressReply {
+  string address = 1;  // address to listen on
+}
+
+// ExportListenerRequest is a request to export the provided listener.
+message ExportListenerRequest {
+  Listener listener = 1;     // listener
+  string local_address = 2;  // LocalAddress argument in ListenerOptions
+}
+
+// ExportListenerReply is a reply to a ExportListenerRequest.
 message ExportListenerReply {
-  int32 port = 1;
-
-  // The address of the proxy that forwards traffic to the returned listener, or
-  // empty if there is no proxy.
-  string proxy_address = 2;
-
-  // True, if the address is already in use.
+  // If non-empty, a non-retriable error.
   //
   // TODO(mwhittaker): This field exists as a hack to avoid a weavelet retrying
   // this error. In the future, we should have a more robust way to avoid
   // retrying certain errors.
-  bool already_in_use = 3;
+  string error = 1;
+
+  // The address of the proxy that forwards traffic to the returned listener, or
+  // empty if there is no proxy.
+  string proxy_address = 2;
 }
 
 // Listener holds information about a network listener.

singleprocess.go
@@ -173,6 +173,10 @@ func (e *singleprocessEnv) GetRoutingInfo(context.Context, string, *call.Version
 	return nil, nil, fmt.Errorf("routing info not useful for singleprocess execution")
 }
 
+func (e *singleprocessEnv) GetAddress(_ context.Context, listener string, opts ListenerOptions) (*protos.GetAddressReply, error) {
+	return &protos.GetAddressReply{Address: opts.LocalAddress}, nil
+}
+
 func (e *singleprocessEnv) ExportListener(_ context.Context, lis *protos.Listener, opts ListenerOptions) (*protos.ExportListenerReply, error) {
 	e.mu.Lock()
 	defer e.mu.Unlock()

weavelet.go
@@ -468,59 +468,24 @@ func (d *weavelet) getInstance(c *component, requester string) (interface{}, err
 // getListener returns a network listener with the given name.
 func (d *weavelet) getListener(name string, opts ListenerOptions) (*Listener, error) {
 	if name == "" {
-		return nil, errors.New("empty listener name")
+		return nil, fmt.Errorf("getListener(%q): empty listener name", name)
 	}
 
-	if d.info.SingleProcess {
-		l, err := net.Listen("tcp", opts.LocalAddress)
-		if err != nil {
-			return nil, err
-		}
-		lis := &protos.Listener{Name: name, Addr: l.Addr().String()}
-		reply, err := d.env.ExportListener(d.ctx, lis, opts)
-		if err != nil {
-			return nil, err
-		}
-		return &Listener{Listener: l, proxyAddr: reply.ProxyAddress}, err
+	// Get the address to listen on.
+	addr, err := d.env.GetAddress(d.ctx, name, opts)
+	if err != nil {
+		return nil, fmt.Errorf("getListener(%q): %w", name, err)
 	}
 
-	lis := &protos.Listener{Name: name}
-	// TODO(mwhittaker): Right now, we resolve our hostname to get a
-	// dialable IP address. Double check that this always works.
-	host := "localhost"
-	if !d.info.UseLocalhost {
-		var err error
-		host, err = os.Hostname()
-		if err != nil {
-			return nil, fmt.Errorf("error getting local hostname: %w", err)
-		}
-	}
-	if d.info.ProcessPicksPorts {
-		l, _, err := d.listen("tcp", fmt.Sprintf("%s:0", host))
-		if err != nil {
-			return nil, err
-		}
-		lis.Addr = l.Addr().String()
-		errMsg := fmt.Sprintf("error exporting listener %v", lis.Addr)
-		var reply *protos.ExportListenerReply
-		if err := d.repeatedly(errMsg, func() error {
-			// TODO(mwhittaker): Don't repeat this operation if the proxy
-			// address is unavailable. Repeating it will likely continue to
-			// fail.
-			var err error
-			reply, err = d.env.ExportListener(d.ctx, lis, opts)
-			return err
-		}); err != nil {
-			return nil, err
-		}
-		if reply.AlreadyInUse {
-			return nil, fmt.Errorf("listener %q: %v already in use", name, opts.LocalAddress)
-		}
-		return &Listener{Listener: l, proxyAddr: reply.ProxyAddress}, nil
+	// Listen on the address.
+	l, err := net.Listen("tcp", addr.Address)
+	if err != nil {
+		return nil, fmt.Errorf("getListener(%q): %w", name, err)
 	}
 
-	// Process doesn't pick the port.
-	const errMsg = "error exporting listener for auto-assigned port"
+	// Export the listener.
+	lis := &protos.Listener{Name: name, Addr: l.Addr().String()}
+	errMsg := fmt.Sprintf("getListener(%q): error exporting listener %v", name, lis.Addr)
 	var reply *protos.ExportListenerReply
 	if err := d.repeatedly(errMsg, func() error {
 		var err error
@@ -529,8 +494,10 @@ func (d *weavelet) getListener(name string, opts ListenerOptions) (*Listener, er
 	}); err != nil {
 		return nil, err
 	}
-	l, _, err := d.listen("tcp", fmt.Sprintf("%s:%d", host, int(reply.Port)))
-	return &Listener{Listener: l, proxyAddr: reply.ProxyAddress}, err
+	if reply.Error != "" {
+		return nil, fmt.Errorf("getListener(%q): %s", name, reply.Error)
+	}
+	return &Listener{Listener: l, proxyAddr: reply.ProxyAddress}, nil
 }
 
 // inLocalColocGroup returns whether the component is hosted in the same colocation

weavertest/multi.go
@@ -133,8 +133,6 @@ func createDeployment(t testing.TB, config string) *protos.Deployment {
 	dep := &protos.Deployment{
 		Id:                uuid.New().String(),
 		App:               appConfig,
-		UseLocalhost:      true,
-		ProcessPicksPorts: true,
 		NetworkStorageDir: t.TempDir(),
 	}
 	return dep
@@ -154,8 +152,6 @@ func createWeaveletForMain(dep *protos.Deployment) *protos.WeaveletInfo {
 		SameProcess:       dep.App.SameProcess,
 		Sections:          dep.App.Sections,
 		SingleProcess:     dep.SingleProcess,
-		UseLocalhost:      dep.UseLocalhost,
-		ProcessPicksPorts: dep.ProcessPicksPorts,
 		NetworkStorageDir: dep.NetworkStorageDir,
 	}
 	return wlet

internal/envelope/conn/conn_test.go
@@ -158,6 +158,9 @@ func (h *handlerForTest) GetRoutingInfo(*protos.GetRoutingInfo) (*protos.Routing
 func (h *handlerForTest) GetComponentsToStart(*protos.GetComponentsToStart) (*protos.ComponentsToStart, error) {
 	return nil, nil
 }
-func (h *handlerForTest) ExportListener(*protos.ListenerToExport) (*protos.ExportListenerReply, error) {
+func (h *handlerForTest) GetAddress(*protos.GetAddressRequest) (*protos.GetAddressReply, error) {
+	return nil, nil
+}
+func (h *handlerForTest) ExportListener(*protos.ExportListenerRequest) (*protos.ExportListenerReply, error) {
 	return nil, nil
 }

runtime/envelope/envelope_test.go
@@ -200,7 +200,10 @@ func (h *handlerForTest) GetRoutingInfo(*protos.GetRoutingInfo) (*protos.Routing
 func (h *handlerForTest) GetComponentsToStart(*protos.GetComponentsToStart) (*protos.ComponentsToStart, error) {
 	return nil, nil
 }
-func (h *handlerForTest) ExportListener(*protos.ListenerToExport) (*protos.ExportListenerReply, error) {
+func (h *handlerForTest) GetAddress(*protos.GetAddressRequest) (*protos.GetAddressReply, error) {
+	return nil, nil
+}
+func (h *handlerForTest) ExportListener(*protos.ExportListenerRequest) (*protos.ExportListenerReply, error) {
 	return nil, nil
 }
 

commit c1a2f1b8c0d97f70b6358e6bff015ec6ea6757f7

Fixed UseLocalhost bug introduced in PR #115.
Before PR #115, the WeaveletInfo proto had a UseLocalhost field. When
true, weavelets would listen on localhost for external listeners (the
kind returned by the Instance.Listener method). In PR #115, I removed
the UseLocalhost field and replaced it with a GetAddress API.

However, I didn't actually remove the field, and the field was being
used for more than I initially thought. The UseLocalhost field was still
in WeaveletInfo and was being used for internal listeners (the kind that
a weavelet uses to execute RPCs). The PR changed 'weaver multi' to not
set the UseLocalhost field which led to weavelets listening on $HOSTNAME
rather than localhost.

This PR fixes the bug. I replace UseLocalhost with SingleMachine since
it parallels the existing SingleProcess field better and I think it's a
bit clearer (use localhost for what?).

I also removed some related unused fields from the WeaveletInfo proto.

Fixes #154.

internal/babysitter/babysitter.go
@@ -164,6 +164,7 @@ func (b *Babysitter) startColocationGroup(group *protos.ColocationGroup) error {
 			SameProcess:   b.dep.App.SameProcess,
 			Sections:      b.dep.App.Sections,
 			SingleProcess: b.dep.SingleProcess,
+			SingleMachine: true,
 		}
 		e, err := envelope.NewEnvelope(wlet, b.dep.App, b, b.opts)
 		if err != nil {

runtime/protos/runtime.pb.go
@@ -987,12 +987,10 @@ type WeaveletInfo struct {
 	GroupId      string           `protobuf:"bytes,4,opt,name=group_id,json=groupId,proto3" json:"group_id,omitempty"`                // colocation group replica id
 	Id           string           `protobuf:"bytes,5,opt,name=id,proto3" json:"id,omitempty"`                                         // weavelet id, in UUID format
 	// TODO(spetrovic): Rename to same_group.
-	SameProcess        []*ComponentGroup `protobuf:"bytes,6,rep,name=same_process,json=sameProcess,proto3" json:"same_process,omitempty"`                                                                // See AppConfig.SameProcess.
-	Sections           map[string]string `protobuf:"bytes,7,rep,name=sections,proto3" json:"sections,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"` // See AppConfig.Sections.
-	SingleProcess      bool              `protobuf:"varint,8,opt,name=single_process,json=singleProcess,proto3" json:"single_process,omitempty"`                                                         // a single-process deployment?
-	UseLocalhost       bool              `protobuf:"varint,9,opt,name=use_localhost,json=useLocalhost,proto3" json:"use_localhost,omitempty"`                                                            // listeners listen on localhost?
-	WeaveletPicksPorts bool              `protobuf:"varint,10,opt,name=weavelet_picks_ports,json=weaveletPicksPorts,proto3" json:"weavelet_picks_ports,omitempty"`                                       // a weavelet picks its own port?
-	NetworkStorageDir  string            `protobuf:"bytes,11,opt,name=network_storage_dir,json=networkStorageDir,proto3" json:"network_storage_dir,omitempty"`                                           // colocation group storage dir
+	SameProcess   []*ComponentGroup `protobuf:"bytes,6,rep,name=same_process,json=sameProcess,proto3" json:"same_process,omitempty"`                                                                // See AppConfig.SameProcess.
+	Sections      map[string]string `protobuf:"bytes,7,rep,name=sections,proto3" json:"sections,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"` // See AppConfig.Sections.
+	SingleProcess bool              `protobuf:"varint,8,opt,name=single_process,json=singleProcess,proto3" json:"single_process,omitempty"`                                                         // a single-process deployment?
+	SingleMachine bool              `protobuf:"varint,9,opt,name=single_machine,json=singleMachine,proto3" json:"single_machine,omitempty"`                                                         // a single-machine deployment?
 }
 
 func (x *WeaveletInfo) Reset() {
@@ -1083,25 +1081,11 @@ func (x *WeaveletInfo) GetSingleProcess() bool {
 	return false
 }
 
-func (x *WeaveletInfo) GetUseLocalhost() bool {
+func (x *WeaveletInfo) GetSingleMachine() bool {
 	if x != nil {
-		return x.UseLocalhost
+		return x.SingleMachine
 	}
 	return false
-}
-
-func (x *WeaveletInfo) GetWeaveletPicksPorts() bool {
-	if x != nil {
-		return x.WeaveletPicksPorts
-	}
-	return false
-}
-
-func (x *WeaveletInfo) GetNetworkStorageDir() string {
-	if x != nil {
-		return x.NetworkStorageDir
-	}
-	return ""
 }
 
 // GetRoutingInfo is a request to retrieve routing information for a given
@@ -3954,7 +3938,7 @@ var file_runtime_protos_runtime_proto_rawDesc = []byte{
 	0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x22, 0x25, 0x0a, 0x0f,
 	0x43, 0x6f, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x47, 0x72, 0x6f, 0x75, 0x70, 0x12,
 	0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
-	0x61, 0x6d, 0x65, 0x22, 0x88, 0x04, 0x0a, 0x0c, 0x57, 0x65, 0x61, 0x76, 0x65, 0x6c, 0x65, 0x74,
+	0x61, 0x6d, 0x65, 0x22, 0xa8, 0x03, 0x0a, 0x0c, 0x57, 0x65, 0x61, 0x76, 0x65, 0x6c, 0x65, 0x74,
 	0x49, 0x6e, 0x66, 0x6f, 0x12, 0x10, 0x0a, 0x03, 0x61, 0x70, 0x70, 0x18, 0x01, 0x20, 0x01, 0x28,
 	0x09, 0x52, 0x03, 0x61, 0x70, 0x70, 0x12, 0x23, 0x0a, 0x0d, 0x64, 0x65, 0x70, 0x6c, 0x6f, 0x79,
 	0x6d, 0x65, 0x6e, 0x74, 0x5f, 0x69, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0c, 0x64,
@@ -3974,16 +3958,10 @@ var file_runtime_protos_runtime_proto_rawDesc = []byte{
 	0x69, 0x6f, 0x6e, 0x73, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x52, 0x08, 0x73, 0x65, 0x63, 0x74, 0x69,
 	0x6f, 0x6e, 0x73, 0x12, 0x25, 0x0a, 0x0e, 0x73, 0x69, 0x6e, 0x67, 0x6c, 0x65, 0x5f, 0x70, 0x72,
 	0x6f, 0x63, 0x65, 0x73, 0x73, 0x18, 0x08, 0x20, 0x01, 0x28, 0x08, 0x52, 0x0d, 0x73, 0x69, 0x6e,
-	0x67, 0x6c, 0x65, 0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x12, 0x23, 0x0a, 0x0d, 0x75, 0x73,
-	0x65, 0x5f, 0x6c, 0x6f, 0x63, 0x61, 0x6c, 0x68, 0x6f, 0x73, 0x74, 0x18, 0x09, 0x20, 0x01, 0x28,
-	0x08, 0x52, 0x0c, 0x75, 0x73, 0x65, 0x4c, 0x6f, 0x63, 0x61, 0x6c, 0x68, 0x6f, 0x73, 0x74, 0x12,
-	0x30, 0x0a, 0x14, 0x77, 0x65, 0x61, 0x76, 0x65, 0x6c, 0x65, 0x74, 0x5f, 0x70, 0x69, 0x63, 0x6b,
-	0x73, 0x5f, 0x70, 0x6f, 0x72, 0x74, 0x73, 0x18, 0x0a, 0x20, 0x01, 0x28, 0x08, 0x52, 0x12, 0x77,
-	0x65, 0x61, 0x76, 0x65, 0x6c, 0x65, 0x74, 0x50, 0x69, 0x63, 0x6b, 0x73, 0x50, 0x6f, 0x72, 0x74,
-	0x73, 0x12, 0x2e, 0x0a, 0x13, 0x6e, 0x65, 0x74, 0x77, 0x6f, 0x72, 0x6b, 0x5f, 0x73, 0x74, 0x6f,
-	0x72, 0x61, 0x67, 0x65, 0x5f, 0x64, 0x69, 0x72, 0x18, 0x0b, 0x20, 0x01, 0x28, 0x09, 0x52, 0x11,
-	0x6e, 0x65, 0x74, 0x77, 0x6f, 0x72, 0x6b, 0x53, 0x74, 0x6f, 0x72, 0x61, 0x67, 0x65, 0x44, 0x69,
-	0x72, 0x1a, 0x3b, 0x0a, 0x0d, 0x53, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x45, 0x6e, 0x74,
+	0x67, 0x6c, 0x65, 0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x12, 0x25, 0x0a, 0x0e, 0x73, 0x69,
+	0x6e, 0x67, 0x6c, 0x65, 0x5f, 0x6d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x18, 0x09, 0x20, 0x01,
+	0x28, 0x08, 0x52, 0x0d, 0x73, 0x69, 0x6e, 0x67, 0x6c, 0x65, 0x4d, 0x61, 0x63, 0x68, 0x69, 0x6e,
+	0x65, 0x1a, 0x3b, 0x0a, 0x0d, 0x53, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x45, 0x6e, 0x74,
 	0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
 	0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20,
 	0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x22, 0x77,

runtime/protos/runtime.proto
@@ -204,9 +204,7 @@ message WeaveletInfo {
   repeated ComponentGroup same_process = 6;  // See AppConfig.SameProcess.
   map<string, string> sections = 7;          // See AppConfig.Sections.
   bool single_process = 8;                   // a single-process deployment?
-  bool use_localhost = 9;                    // listeners listen on localhost?
-  bool weavelet_picks_ports = 10;            // a weavelet picks its own port?
-  string network_storage_dir = 11;           // colocation group storage dir
+  bool single_machine = 9;                   // a single-machine deployment?
 }
 
 // GetRoutingInfo is a request to retrieve routing information for a given

runtime/weavelet.go
@@ -44,5 +44,8 @@ func CheckWeaveletInfo(w *protos.WeaveletInfo) error {
 	if _, err := uuid.Parse(w.Id); err != nil {
 		return fmt.Errorf("WeaveletInfo: invalid weavelet id: %w", err)
 	}
+	if w.SingleProcess && !w.SingleMachine {
+		return fmt.Errorf("WeaveletInfo: single process but not single machine")
+	}
 	return nil
 }

singleprocess.go
@@ -98,16 +98,15 @@ func newSingleprocessEnv(bootstrap runtime.Bootstrap) (*singleprocessEnv, error)
 	appConfig.Args = os.Args[1:]
 
 	wlet := &protos.WeaveletInfo{
-		App:                appConfig.Name,
-		DeploymentId:       uuid.New().String(),
-		Group:              &protos.ColocationGroup{Name: "main"},
-		GroupId:            uuid.New().String(),
-		Id:                 uuid.New().String(),
-		SameProcess:        appConfig.SameProcess,
-		Sections:           appConfig.Sections,
-		SingleProcess:      true,
-		UseLocalhost:       true,
-		WeaveletPicksPorts: true,
+		App:           appConfig.Name,
+		DeploymentId:  uuid.New().String(),
+		Group:         &protos.ColocationGroup{Name: "main"},
+		GroupId:       uuid.New().String(),
+		Id:            uuid.New().String(),
+		SameProcess:   appConfig.SameProcess,
+		Sections:      appConfig.Sections,
+		SingleProcess: true,
+		SingleMachine: true,
 	}
 	if err := runtime.CheckWeaveletInfo(wlet); err != nil {
 		return nil, err

weavelet.go
@@ -213,7 +213,7 @@ func (d *weavelet) start() (Instance, error) {
 		// TODO(mwhittaker): Right now, we resolve our hostname to get a
 		// dialable IP address. Double check that this always works.
 		host := "localhost"
-		if !d.info.UseLocalhost {
+		if !d.info.SingleMachine {
 			var err error
 			host, err = os.Hostname()
 			if err != nil {

weavertest/multi.go
@@ -213,6 +213,7 @@ func createWeaveletForMain(dep *protos.Deployment) *protos.WeaveletInfo {
 		SameProcess:   dep.App.SameProcess,
 		Sections:      dep.App.Sections,
 		SingleProcess: dep.SingleProcess,
+		SingleMachine: true,
 	}
 	return wlet
 }

internal/envelope/conn/conn_test.go
@@ -99,11 +99,13 @@ func makeConnections(t *testing.T, handler conn.EnvelopeHandler) (*conn.Envelope
 
 	// Construct the conns.
 	wlet := &protos.WeaveletInfo{
-		App:          "app",
-		DeploymentId: uuid.New().String(),
-		Group:        &protos.ColocationGroup{Name: "group"},
-		GroupId:      uuid.New().String(),
-		Id:           uuid.New().String(),
+		App:           "app",
+		DeploymentId:  uuid.New().String(),
+		Group:         &protos.ColocationGroup{Name: "group"},
+		GroupId:       uuid.New().String(),
+		Id:            uuid.New().String(),
+		SingleProcess: true,
+		SingleMachine: true,
 	}
 	e, err := conn.NewEnvelopeConn(eReader, eWriter, handler, wlet)
 	if err != nil {

runtime/envelope/envelope_test.go
@@ -129,11 +129,13 @@ func checkFile() error {
 // wlet returns a WeaveletInfo and AppConfig for testing.
 func wlet(binary string, args ...string) (*protos.WeaveletInfo, *protos.AppConfig) {
 	weavelet := &protos.WeaveletInfo{
-		App:          "app",
-		DeploymentId: uuid.New().String(),
-		Group:        &protos.ColocationGroup{Name: "main"},
-		GroupId:      uuid.New().String(),
-		Id:           uuid.New().String(),
+		App:           "app",
+		DeploymentId:  uuid.New().String(),
+		Group:         &protos.ColocationGroup{Name: "main"},
+		GroupId:       uuid.New().String(),
+		Id:            uuid.New().String(),
+		SingleProcess: true,
+		SingleMachine: true,
 	}
 	config := &protos.AppConfig{Binary: binary, Args: args}
 	return weavelet, config