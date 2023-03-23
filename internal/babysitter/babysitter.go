ServiceWeaver/weaver

commit 14d31673e9caf0e8d934f7eb3c5dacbd45d4febe

Remove RegisterReplica method from the envelope <-> weavelet protocol. (#184)
Instead, we get the weavelet's runtime information immediately after
creating the weavelet.

Suggested by mwhittaker@.

Other changes:
  * Minor Envelope cleanup.

internal/babysitter/babysitter.go
@@ -253,7 +253,7 @@ func (b *Babysitter) startColocationGroup(g *group) error {
 
 	for r := 0; r < defaultReplication; r++ {
 		// Start the weavelet and capture its logs, traces, and metrics.
-		wlet := &protos.WeaveletInfo{
+		wlet := &protos.WeaveletSetupInfo{
 			App:           b.dep.App.Name,
 			DeploymentId:  b.dep.Id,
 			Group:         &protos.ColocationGroup{Name: g.name},
@@ -265,12 +265,15 @@ func (b *Babysitter) startColocationGroup(g *group) error {
 			SingleMachine: true,
 		}
 		h := &handler{b, g}
-		e, err := envelope.NewEnvelope(wlet, b.dep.App, h)
+		e, err := envelope.NewEnvelope(b.ctx, wlet, b.dep.App, h)
 		if err != nil {
 			return err
 		}
+		if err := b.registerReplica(g, e.WeaveletInfo()); err != nil {
+			return err
+		}
 		go func() {
-			if err := e.Run(b.ctx); err != nil {
+			if err := e.Serve(b.ctx); err != nil {
 				b.done <- err
 			}
 		}()
@@ -320,25 +323,17 @@ func (b *Babysitter) StartComponent(req *protos.ComponentToStart) error {
 	return b.startColocationGroup(g)
 }
 
-// RegisterReplica implements the envelope.EnvelopeHandler interface.
-func (b *Babysitter) RegisterReplica(req *protos.ReplicaToRegister) error {
-	g := b.group(req.Group)
-
+// registerReplica registers the information about a colocation group replica
+// (i.e., a weavelet).
+// REQUIRES: g.mu is held.
+func (b *Babysitter) registerReplica(g *group, info *protos.WeaveletInfo) error {
 	// Update addresses and pids.
-	record := func() bool {
-		g.mu.Lock()
-		defer g.mu.Unlock()
-		if g.addresses[req.Address] {
-			// Replica already registered.
-			return true
-		}
-		g.addresses[req.Address] = true
-		g.pids = append(g.pids, req.Pid)
-		return false
-	}
-	if record() {
+	if g.addresses[info.DialAddr] {
+		// Replica already registered.
 		return nil
 	}
+	g.addresses[info.DialAddr] = true
+	g.pids = append(g.pids, info.Pid)
 
 	// Update routing.
 	replicas := g.allAddresses()
@@ -350,6 +345,7 @@ func (b *Babysitter) RegisterReplica(req *protos.ReplicaToRegister) error {
 		}
 		routing.Unlock()
 	}
+
 	return nil
 }
 

internal/tool/ssh/impl/babysitter.go
@@ -37,9 +37,7 @@ import (
 // group for a single application version, on the local machine.
 type babysitter struct {
 	ctx           context.Context
-	dep           *protos.Deployment
 	info          *BabysitterInfo
-	mgrAddr       string
 	logger        logtype.Logger
 	traceExporter *traceio.Writer // to export traces to the manager
 }
@@ -64,10 +62,8 @@ func RunBabysitter(ctx context.Context) error {
 
 	id := uuid.New().String()
 	b := &babysitter{
-		ctx:     ctx,
-		dep:     info.Deployment,
-		info:    info,
-		mgrAddr: info.ManagerAddr,
+		ctx:  ctx,
+		info: info,
 		logger: logging.FuncLogger{
 			Opts: logging.Options{
 				App:        info.Deployment.App.Name,
@@ -89,23 +85,26 @@ func RunBabysitter(ctx context.Context) error {
 	}
 
 	// Start the envelope.
-	wlet := &protos.WeaveletInfo{
-		App:           b.dep.App.Name,
-		DeploymentId:  b.dep.Id,
+	wlet := &protos.WeaveletSetupInfo{
+		App:           info.Deployment.App.Name,
+		DeploymentId:  info.Deployment.Id,
 		Group:         info.Group,
 		GroupId:       id,
 		Id:            id,
-		SameProcess:   b.dep.App.SameProcess,
-		Sections:      b.dep.App.Sections,
-		SingleProcess: b.dep.SingleProcess,
+		SameProcess:   info.Deployment.App.SameProcess,
+		Sections:      info.Deployment.App.Sections,
+		SingleProcess: info.Deployment.SingleProcess,
 	}
-	e, err := envelope.NewEnvelope(wlet, b.dep.App, b)
+	e, err := envelope.NewEnvelope(ctx, wlet, info.Deployment.App, b)
 	if err != nil {
+		return err
+	}
+	if err := b.registerReplica(e.WeaveletInfo()); err != nil {
 		return err
 	}
 	c := metricsCollector{logger: b.logger, envelope: e, info: info}
 	go c.run(ctx)
-	return e.Run(ctx)
+	return e.Serve(ctx)
 }
 
 type metricsCollector struct {
@@ -154,22 +153,24 @@ func (m *metricsCollector) run(ctx context.Context) {
 func (b *babysitter) StartComponent(req *protos.ComponentToStart) error {
 	return protomsg.Call(b.ctx, protomsg.CallArgs{
 		Client:  http.DefaultClient,
-		Addr:    b.mgrAddr,
+		Addr:    b.info.ManagerAddr,
 		URLPath: startComponentURL,
 		Request: req,
 	})
 }
 
-// RegisterReplica implements the protos.EnvelopeHandler interface.
-func (b *babysitter) RegisterReplica(replica *protos.ReplicaToRegister) error {
-	b.logger.Info("Replica (re)started with new address",
-		"group", logging.ShortenComponent(replica.Group),
-		"address", replica.Address)
+// registerReplica registers the information about a colocation group replica
+// (i.e., a weavelet).
+func (b *babysitter) registerReplica(info *protos.WeaveletInfo) error {
 	return protomsg.Call(b.ctx, protomsg.CallArgs{
 		Client:  http.DefaultClient,
-		Addr:    b.mgrAddr,
+		Addr:    b.info.ManagerAddr,
 		URLPath: registerReplicaURL,
-		Request: replica,
+		Request: &ReplicaToRegister{
+			Group:   b.info.Group.Name,
+			Address: info.DialAddr,
+			Pid:     info.Pid,
+		},
 	})
 }
 
@@ -191,7 +192,7 @@ func (b *babysitter) ExportListener(req *protos.ExportListenerRequest) (*protos.
 	reply := &protos.ExportListenerReply{}
 	if err := protomsg.Call(b.ctx, protomsg.CallArgs{
 		Client:  http.DefaultClient,
-		Addr:    b.mgrAddr,
+		Addr:    b.info.ManagerAddr,
 		URLPath: exportListenerURL,
 		Request: req,
 		Reply:   reply,
@@ -206,7 +207,7 @@ func (b *babysitter) GetRoutingInfo(req *protos.GetRoutingInfo) (*protos.Routing
 	reply := &protos.RoutingInfo{}
 	if err := protomsg.Call(b.ctx, protomsg.CallArgs{
 		Client:  http.DefaultClient,
-		Addr:    b.mgrAddr,
+		Addr:    b.info.ManagerAddr,
 		URLPath: getRoutingInfoURL,
 		Request: req,
 		Reply:   reply,
@@ -222,7 +223,7 @@ func (b *babysitter) GetComponentsToStart(get *protos.GetComponentsToStart) (*pr
 	reply := &protos.ComponentsToStart{}
 	err := protomsg.Call(b.ctx, protomsg.CallArgs{
 		Client:  http.DefaultClient,
-		Addr:    b.mgrAddr,
+		Addr:    b.info.ManagerAddr,
 		URLPath: getComponentsToStartURL,
 		Request: req,
 		Reply:   reply,
@@ -234,12 +235,12 @@ func (b *babysitter) GetComponentsToStart(get *protos.GetComponentsToStart) (*pr
 func (b *babysitter) RecvLogEntry(req *protos.LogEntry) {
 	err := protomsg.Call(b.ctx, protomsg.CallArgs{
 		Client:  http.DefaultClient,
-		Addr:    b.mgrAddr,
+		Addr:    b.info.ManagerAddr,
 		URLPath: recvLogEntryURL,
 		Request: req,
 	})
 	if err != nil {
-		b.logger.Error("Error receiving logs", err, "fromAddr", b.mgrAddr)
+		b.logger.Error("Error receiving logs", err, "fromAddr", b.info.ManagerAddr)
 	}
 }
 

env.go
@@ -16,6 +16,7 @@ package weaver
 
 import (
 	"context"
+	"net"
 
 	"github.com/ServiceWeaver/weaver/internal/logtype"
 	"github.com/ServiceWeaver/weaver/internal/net/call"
@@ -27,8 +28,13 @@ import (
 // env provides the API by which a Service Weaver application process communicates with
 // its execution environment, e.g., to do thing like starting processes etc.
 type env interface {
-	// GetWeaveletInfo returns the weavelet information from the environment.
-	GetWeaveletInfo() *protos.WeaveletInfo
+	// WeaveletSetupInfo returns the weavelet's setup information sent by
+	// the deployer.
+	WeaveletSetupInfo() *protos.WeaveletSetupInfo
+
+	// WeaveletListener returns the internal listener the weavelet should
+	// listen on to receive messages from other weavelets.
+	WeaveletListener() net.Listener
 
 	// RegisterComponentToStart registers a component to start in a given
 	// target colocation group.
@@ -43,15 +49,8 @@ type env interface {
 	// to the set of components returned.
 	GetComponentsToStart(ctx context.Context, version *call.Version) ([]string, *call.Version, error)
 
-	// RegisterReplica registers this process's address. This allows other processes
-	// in the application to make calls to components in this process.
-	RegisterReplica(ctx context.Context, myAddress call.NetworkAddress) error
-
-	// ReportLoad reports load information for a weavelet.
-	ReportLoad(ctx context.Context, load *protos.WeaveletLoadReport) error
-
-	// GetRoutingInfo returns the routing info for the provided component,
-	// including the set of replicas and the current routing assignments
+	// GetRoutingInfo returns the routing info for the provided colocation
+	// group, including the set of replicas and the current routing assignments
 	// (if any).
 	GetRoutingInfo(ctx context.Context, component string, version *call.Version) (*protos.RoutingInfo, *call.Version, error)
 

internal/envelope/conn/conn.go
@@ -21,9 +21,9 @@ import (
 	"io"
 	sync "sync"
 
-	"google.golang.org/protobuf/proto"
 	"github.com/ServiceWeaver/weaver/runtime/protomsg"
 	"github.com/ServiceWeaver/weaver/runtime/protos"
+	"google.golang.org/protobuf/proto"
 )
 
 // conn is a bi-directional communication channel that is used in the

internal/envelope/conn/conn_test.go
@@ -98,7 +98,7 @@ func makeConnections(t *testing.T, handler conn.EnvelopeHandler) (*conn.Envelope
 	}
 
 	// Construct the conns.
-	wlet := &protos.WeaveletInfo{
+	wlet := &protos.WeaveletSetupInfo{
 		App:           "app",
 		DeploymentId:  uuid.New().String(),
 		Group:         &protos.ColocationGroup{Name: "group"},
@@ -107,37 +107,46 @@ func makeConnections(t *testing.T, handler conn.EnvelopeHandler) (*conn.Envelope
 		SingleProcess: true,
 		SingleMachine: true,
 	}
-	e, err := conn.NewEnvelopeConn(eReader, eWriter, handler, wlet)
-	if err != nil {
-		t.Fatal(err)
-	}
-	w, err := conn.NewWeaveletConn(wReader, wWriter, nil /*handler*/)
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	// Start Run goroutines for both conns.
-	wait := &sync.WaitGroup{}
-	wait.Add(2)
+	started := &sync.WaitGroup{}
+	started.Add(2)
+	done := &sync.WaitGroup{}
+	done.Add(2)
+	var e *conn.EnvelopeConn
 	go func() {
-		defer wait.Done()
-		if err := e.Run(); err != nil && !errors.Is(err, io.EOF) && !strings.Contains(err.Error(), "file already closed") {
+		defer done.Done()
+		var err error
+		if e, err = conn.NewEnvelopeConn(eReader, eWriter, handler, wlet); err != nil {
+			panic(err)
+		}
+		started.Done()
+		if err := e.Serve(); err != nil && !errors.Is(err, io.EOF) && !strings.Contains(err.Error(), "file already closed") {
 			t.Errorf("envelope failed: %#v", err)
 		}
 	}()
+	var w *conn.WeaveletConn
 	go func() {
-		defer wait.Done()
-		if err := w.Run(); err != nil && !errors.Is(err, io.EOF) && !strings.Contains(err.Error(), "file already closed") {
+		defer done.Done()
+		var err error
+		if w, err = conn.NewWeaveletConn(wReader, wWriter, nil /*handler*/); err != nil {
+			panic(err)
+		}
+		started.Done()
+		if err := w.Serve(); err != nil && !errors.Is(err, io.EOF) && !strings.Contains(err.Error(), "file already closed") {
 			t.Errorf("weavelet failed: %#v", err)
 		}
 	}()
+
+	// Wait for both conns to start.
+	started.Wait()
 
 	// Stop goroutines when test has finished.
 	t.Cleanup(func() {
 		wReader.Close()
 		eReader.Close()
 		// NOTE(mwhittaker): wWriter and eWriter are closed by the conns.
-		wait.Wait()
+		done.Wait()
 	})
 
 	return e, w
@@ -147,11 +156,10 @@ type handlerForTest struct{}
 
 var _ conn.EnvelopeHandler = &handlerForTest{}
 
-func (h *handlerForTest) RecvTraceSpans([]trace.ReadOnlySpan) error       { return nil }
-func (h *handlerForTest) RecvLogEntry(*protos.LogEntry)                   {}
-func (h *handlerForTest) StartComponent(*protos.ComponentToStart) error   { return nil }
-func (h *handlerForTest) RegisterReplica(*protos.ReplicaToRegister) error { return nil }
-func (h *handlerForTest) ReportLoad(*protos.WeaveletLoadReport) error     { return nil }
+func (h *handlerForTest) RecvTraceSpans([]trace.ReadOnlySpan) error     { return nil }
+func (h *handlerForTest) RecvLogEntry(*protos.LogEntry)                 {}
+func (h *handlerForTest) StartComponent(*protos.ComponentToStart) error { return nil }
+func (h *handlerForTest) ReportLoad(*protos.WeaveletLoadReport) error   { return nil }
 func (h *handlerForTest) GetRoutingInfo(*protos.GetRoutingInfo) (*protos.RoutingInfo, error) {
 	return nil, nil
 }

internal/envelope/conn/envelope_conn.go
@@ -31,9 +31,6 @@ type EnvelopeHandler interface {
 	// StartComponent starts the given component.
 	StartComponent(entry *protos.ComponentToStart) error
 
-	// RegisterReplica registers the given weavelet replica.
-	RegisterReplica(entry *protos.ReplicaToRegister) error
-
 	// GetAddress gets the address a weavelet should listen on for a listener.
 	GetAddress(req *protos.GetAddressRequest) (*protos.GetAddressReply, error)
 
@@ -63,9 +60,10 @@ type EnvelopeHandler interface {
 // EnvelopeConn is the envelope side of the connection between a weavelet
 // and the envelope.
 type EnvelopeConn struct {
-	handler EnvelopeHandler
-	conn    conn
-	metrics metrics.Importer
+	handler  EnvelopeHandler
+	conn     conn
+	metrics  metrics.Importer
+	weavelet *protos.WeaveletInfo
 }
 
 // NewEnvelopeConn creates the envelope side of the connection between a
@@ -73,20 +71,34 @@ type EnvelopeConn struct {
 // Synthesized high-level events are passed to h.
 //
 // NewEnvelopeConn sends the provided protos.Weavelet to the weavelet.
-func NewEnvelopeConn(r io.ReadCloser, w io.WriteCloser, h EnvelopeHandler, weavelet *protos.WeaveletInfo) (*EnvelopeConn, error) {
+func NewEnvelopeConn(r io.ReadCloser, w io.WriteCloser, h EnvelopeHandler, wlet *protos.WeaveletSetupInfo) (*EnvelopeConn, error) {
 	e := &EnvelopeConn{
 		handler: h,
 		conn:    conn{name: "envelope", reader: r, writer: w},
 	}
-
-	req := &protos.EnvelopeMsg{WeaveletInfo: weavelet}
-	err := e.send(req)
-	return e, err
+	// Send the setup information to the weavelet, and receive the weavelet
+	// information in return.
+	if err := e.send(&protos.EnvelopeMsg{WeaveletSetupInfo: wlet}); err != nil {
+		return nil, err
+	}
+	reply := &protos.WeaveletMsg{}
+	if err := e.conn.recv(reply); err != nil {
+		e.conn.cleanup(err)
+		return nil, err
+	}
+	if reply.WeaveletInfo == nil {
+		err := fmt.Errorf(
+			"the first message from the weavelet must contain weavelet info")
+		e.conn.cleanup(err)
+		return nil, err
+	}
+	e.weavelet = reply.WeaveletInfo
+	return e, nil
 }
 
-// Run interacts with the peer. Messages that are received are
-// handled as an ordered sequence.
-func (e *EnvelopeConn) Run() error {
+// Serve accepts incoming messages from the weavelet. Messages that are received
+// are handled as an ordered sequence.
+func (e *EnvelopeConn) Serve() error {
 	msg := &protos.WeaveletMsg{}
 	for {
 		if err := e.conn.recv(msg); err != nil {
@@ -96,6 +108,11 @@ func (e *EnvelopeConn) Run() error {
 			return err
 		}
 	}
+}
+
+// WeaveletInfo returns the information about the weavelet.
+func (e *EnvelopeConn) WeaveletInfo() *protos.WeaveletInfo {
+	return e.weavelet
 }
 
 // handleMessage handles all messages initiated by the weavelet. Note that
@@ -111,8 +128,6 @@ func (e *EnvelopeConn) handleMessage(msg *protos.WeaveletMsg) error {
 	switch {
 	case msg.ComponentToStart != nil:
 		return e.send(errReply(e.handler.StartComponent(msg.ComponentToStart)))
-	case msg.ReplicaToRegister != nil:
-		return e.send(errReply(e.handler.RegisterReplica(msg.ReplicaToRegister)))
 	case msg.GetAddressRequest != nil:
 		reply, err := e.handler.GetAddress(msg.GetAddressRequest)
 		if err != nil {

internal/envelope/conn/trace_readwrite_test.go
@@ -47,10 +47,9 @@ func (p *pipeForTest) RecvTraceSpans(spans []sdk.ReadOnlySpan) error {
 	return nil
 }
 
-func (p *pipeForTest) RecvLogEntry(*protos.LogEntry)                   {}
-func (p *pipeForTest) StartComponent(*protos.ComponentToStart) error   { return nil }
-func (p *pipeForTest) RegisterReplica(*protos.ReplicaToRegister) error { return nil }
-func (p *pipeForTest) ReportLoad(*protos.WeaveletLoadReport) error     { return nil }
+func (p *pipeForTest) RecvLogEntry(*protos.LogEntry)                 {}
+func (p *pipeForTest) StartComponent(*protos.ComponentToStart) error { return nil }
+func (p *pipeForTest) ReportLoad(*protos.WeaveletLoadReport) error   { return nil }
 func (p *pipeForTest) GetRoutingInfo(*protos.GetRoutingInfo) (*protos.RoutingInfo, error) {
 	return nil, nil
 }

internal/envelope/conn/weavelet_conn.go
@@ -18,6 +18,8 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net"
+	"os"
 	"runtime/pprof"
 	"time"
 
@@ -39,7 +41,8 @@ type WeaveletHandler interface {
 type WeaveletConn struct {
 	handler WeaveletHandler
 	conn    conn
-	wlet    *protos.WeaveletInfo
+	info    *protos.WeaveletSetupInfo
+	lis     net.Listener // internal network listener for the weavelet
 	metrics metrics.Exporter
 }
 
@@ -55,28 +58,41 @@ func NewWeaveletConn(r io.ReadCloser, w io.WriteCloser, h WeaveletHandler) (*Wea
 		conn:    conn{name: "weavelet", reader: r, writer: w},
 	}
 
-	// Block until a weavelet is received.
+	// Block until the weavelet information is received.
 	msg := &protos.EnvelopeMsg{}
 	if err := d.conn.recv(msg); err != nil {
 		d.conn.cleanup(err)
 		return nil, err
 	}
-	d.wlet = msg.WeaveletInfo
-	if d.wlet == nil {
-		err := fmt.Errorf("expected WeaveletInfo, got %v", msg)
+	d.info = msg.WeaveletSetupInfo
+	if d.info == nil {
+		err := fmt.Errorf("expected WeaveletSetupInfo, got %v", msg)
 		d.conn.cleanup(err)
 		return nil, err
 	}
-	if err := runtime.CheckWeaveletInfo(d.wlet); err != nil {
+	if err := runtime.CheckWeaveletSetupInfo(d.info); err != nil {
 		d.conn.cleanup(err)
+		return nil, err
+	}
+
+	// Reply to the envelope with weavelet's runtime information.
+	lis, err := listen(d.info)
+	if err != nil {
+		d.conn.cleanup(err)
+		return nil, err
+	}
+	d.lis = lis
+	dialAddr := fmt.Sprintf("tcp://%s", lis.Addr().String())
+	info := &protos.WeaveletInfo{DialAddr: dialAddr}
+	if err := d.send(&protos.WeaveletMsg{WeaveletInfo: info}); err != nil {
 		return nil, err
 	}
 	return d, nil
 }
 
-// Run interacts with the peer. Messages that are received are
-// handled as an ordered sequence.
-func (d *WeaveletConn) Run() error {
+// Serve accepts incoming messages from the envelope. Messages that are received
+// are handled as an ordered sequence.
+func (d *WeaveletConn) Serve() error {
 	msg := &protos.EnvelopeMsg{}
 	for {
 		if err := d.conn.recv(msg); err != nil {
@@ -88,9 +104,14 @@ func (d *WeaveletConn) Run() error {
 	}
 }
 
-// Weavelet returns the protos.Weavelet for this weavelet.
-func (d *WeaveletConn) Weavelet() *protos.WeaveletInfo {
-	return d.wlet
+// Bootstrap returns the setup information for the weavelet.
+func (d *WeaveletConn) WeaveletSetupInfo() *protos.WeaveletSetupInfo {
+	return d.info
+}
+
+// Listener returns the internal network listener for the weavelet.
+func (d *WeaveletConn) Listener() net.Listener {
+	return d.lis
 }
 
 // handleMessage handles all messages initiated by the envelope. Note that
@@ -104,9 +125,9 @@ func (d *WeaveletConn) handleMessage(msg *protos.EnvelopeMsg) error {
 			if def.Labels == nil {
 				def.Labels = map[string]string{}
 			}
-			def.Labels["serviceweaver_app"] = d.wlet.App
-			def.Labels["serviceweaver_version"] = d.wlet.DeploymentId
-			def.Labels["serviceweaver_node"] = d.wlet.Id
+			def.Labels["serviceweaver_app"] = d.info.App
+			def.Labels["serviceweaver_version"] = d.info.DeploymentId
+			def.Labels["serviceweaver_node"] = d.info.Id
 		}
 		return d.send(&protos.WeaveletMsg{Id: -msg.Id, Metrics: update})
 	case msg.SendHealthStatus:
@@ -151,12 +172,6 @@ func (d *WeaveletConn) handleMessage(msg *protos.EnvelopeMsg) error {
 // StartComponentRPC requests the envelope to start the given component.
 func (d *WeaveletConn) StartComponentRPC(componentToStart *protos.ComponentToStart) error {
 	_, err := d.rpc(&protos.WeaveletMsg{ComponentToStart: componentToStart})
-	return err
-}
-
-// RegisterReplicaRPC requests the envelope to register the given replica.
-func (d *WeaveletConn) RegisterReplicaRPC(replica *protos.ReplicaToRegister) error {
-	_, err := d.rpc(&protos.WeaveletMsg{ReplicaToRegister: replica})
 	return err
 }
 
@@ -272,4 +287,21 @@ func Profile(req *protos.RunProfiling) ([]byte, error) {
 		return nil, fmt.Errorf("unspecified profile collection type")
 	}
 	return buf.Bytes(), nil
+}
+
+func listen(info *protos.WeaveletSetupInfo) (net.Listener, error) {
+	// Pick a hostname to listen on.
+	host := "localhost"
+	if !info.SingleMachine {
+		// TODO(mwhittaker): Right now, we resolve our hostname to get a
+		// dialable IP address. Double check that this always works.
+		var err error
+		host, err = os.Hostname()
+		if err != nil {
+			return nil, fmt.Errorf("error getting local hostname: %w", err)
+		}
+	}
+
+	// Create the listener
+	return net.Listen("tcp", fmt.Sprintf("%s:0", host))
 }

commit 342c5ef04fdeefd93d9292687ceb339f2369355b

Fix the babysitter deadlock caused by double-locking.
This breakage was introduced in my PR:
  https://github.com/ServiceWeaver/weaver/pull/184

internal/babysitter/babysitter.go
@@ -105,6 +105,7 @@ type group struct {
 
 	// guards the following data structures, but not their contents.
 	mu        sync.Mutex
+	started   bool                                                 // Has the group been started?
 	addresses map[string]bool                                      // weavelet addresses
 	envelopes []*envelope.Envelope                                 // envelopes, one per weavelet
 	pids      []int64                                              // weavelet pids
@@ -244,10 +245,16 @@ func (b *Babysitter) allProxies() []*proxyInfo {
 //
 // REQUIRES: g.mu is NOT held.
 func (b *Babysitter) startColocationGroup(g *group) error {
-	g.mu.Lock()
-	defer g.mu.Unlock()
-	if len(g.envelopes) == defaultReplication {
-		// Already started.
+	shouldStart := func() bool {
+		g.mu.Lock()
+		defer g.mu.Unlock()
+		if g.started {
+			return false
+		}
+		g.started = true
+		return true
+	}
+	if !shouldStart() {
 		return nil
 	}
 
@@ -325,15 +332,22 @@ func (b *Babysitter) StartComponent(req *protos.ComponentToStart) error {
 
 // registerReplica registers the information about a colocation group replica
 // (i.e., a weavelet).
-// REQUIRES: g.mu is held.
 func (b *Babysitter) registerReplica(g *group, info *protos.WeaveletInfo) error {
-	// Update addresses and pids.
-	if g.addresses[info.DialAddr] {
-		// Replica already registered.
+	register := func() bool {
+		g.mu.Lock()
+		defer g.mu.Unlock()
+		if g.addresses[info.DialAddr] {
+			// Replica already registered.
+			return false
+		}
+		g.addresses[info.DialAddr] = true
+		g.pids = append(g.pids, info.Pid)
+		return true
+	}
+
+	if !register() { // already registered
 		return nil
 	}
-	g.addresses[info.DialAddr] = true
-	g.pids = append(g.pids, info.Pid)
 
 	// Update routing.
 	replicas := g.allAddresses()