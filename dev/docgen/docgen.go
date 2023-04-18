aad75403fd00cc2544118e7a2f26ed0854bb90c5
Michael Whittaker <mwhittaker@google.com>
2023-04-18 15:27:09 -0700

    Prettified News page on website.

--- dev/docgen/docgen.go
+++ dev/docgen/docgen.go
@@ -60,7 +60,7 @@ var files = []file{
 
 	{dst: "blog/index.html", html: "blog/index.html", template: "blog.html", license: true},
 	{dst: "blog/quick_intro.html", markdown: "blog/quick_intro.md", template: "blog_entry.html", title: "A Quick Introduction to Service Weaver", license: true},
-	{dst: "news.html", markdown: "news.md", template: "news.html", license: true},
+	{dst: "news.html", html: "news.html", template: "news.html", license: true},
 
 	{dst: "blog/deployers.html", markdown: "blog/deployers.md", template: "blog_entry.html", title: "How to Implement a Service Weaver Deployer", license: true},
 	staticFile("blog/deployers/assets/overview.svg"),
@@ -73,6 +73,7 @@ var files = []file{
 	staticFile("assets/css/common.css"),
 	staticFile("assets/css/docs.css"),
 	staticFile("assets/css/home.css"),
+	staticFile("assets/css/news.css"),
 	staticFile("assets/images/cloud_metrics_1.png"),
 	staticFile("assets/images/cloud_metrics_2.png"),
 	staticFile("assets/images/cloud_metrics_3.png"),
