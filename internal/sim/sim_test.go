a06051d4dbfd1142e18e3b2421fe1ecd5207d9d5
Michael Whittaker <mwhittaker@google.com>
2023-07-20 16:57:45 -0700

    Register operations (ops) with simulator.
    This PR introduces an `Op[T]` type to represent operations executed as
    part of a simulation. Currently, the ops are not actually executed; that
    will come in a future PR.
    
    I'm not 100% satisfied with the `Op` API right now. The biggest
    awkwardness comes when your operation wants to receive more than one
    random argument. To do that with this API, you have to wrap all
    arguments into a struct. That is clunky.
    
    Registering an `Op` is also a bit strange because you have to call a
    function rather than a `Sim` method. This is because methods in Go
    cannot be generic.
    
    I'd love to improve the API later if we can think of something better,
    and I'm definitely open to suggestions.

--- internal/sim/sim_test.go
+++ internal/sim/sim_test.go
@@ -16,20 +16,41 @@ package sim
 
 import (
 	"context"
+	"math/rand"
 	"testing"
 
 	"github.com/ServiceWeaver/weaver/internal/reflection"
 )
 
-func TestSim(t *testing.T) {
+func simulator(t *testing.T) *Simulator {
+	t.Helper()
 	sim, err := New(Options{NumReplicas: 2})
 	if err != nil {
 		t.Fatal(err)
 	}
+	return sim
+}
+
+func TestSim(t *testing.T) {
+	type pair struct {
+		x, y int
+	}
+
+	sim := simulator(t)
 	x, err := sim.GetIntf(reflection.Type[swapper]())
 	if err != nil {
 		t.Fatal(err)
 	}
+	RegisterOp(sim, Op[pair]{
+		Name: "swap",
+		Gen: func(r *rand.Rand) pair {
+			return pair{rand.Intn(100), rand.Intn(100)}
+		},
+		Func: func(ctx context.Context, p pair, s swapper) error {
+			_, _, err := s.Swap(ctx, p.x, p.y)
+			return err
+		},
+	})
 	swapper := x.(swapper)
 	x, y, err := swapper.Swap(context.Background(), 1, 2)
 	if x != 2 {
@@ -40,5 +61,75 @@ func TestSim(t *testing.T) {
 	}
 	if err != (swapperError{}) {
 		t.Errorf("err: got %v, want %v", err, swapperError{})
+	}
+}
+
+func TestDuplicateOp(t *testing.T) {
+	sim := simulator(t)
+	sim.ops["foo"] = op{}
+	validateOp(sim, Op[int]{
+		Name: "foo",
+		Gen:  func(*rand.Rand) int { return 42 },
+		Func: func(*rand.Rand) error { return nil },
+	})
+}
+
+func TestValidateOp(t *testing.T) {
+	ops := map[string]Op[int]{
+		"missing name": {
+			Name: "",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(context.Context, int) error { return nil },
+		},
+		"nil gen": {
+			Name: "",
+			Func: func(context.Context, int) error { return nil },
+		},
+		"nil func": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+		},
+		"func is not a function": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: 42,
+		},
+		"func has too few arguments": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(context.Context) error { return nil },
+		},
+		"func has incorrect first argument": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(int, int) error { return nil },
+		},
+		"func has incorrect second argument": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(context.Context, bool) error { return nil },
+		},
+		"func has incorrect third argument": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(context.Context, int, int) error { return nil },
+		},
+		"func has incorrect number of returns": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(context.Context, int) (int, error) { return 42, nil },
+		},
+		"func has incorrect return": {
+			Name: "foo",
+			Gen:  func(*rand.Rand) int { return 42 },
+			Func: func(context.Context, int) int { return 42 },
+		},
+	}
+	for name, op := range ops {
+		t.Run(name, func(t *testing.T) {
+			if _, err := validateOp(simulator(t), op); err == nil {
+				t.Fatal("unexpected success")
+			}
+		})
 	}
 }
