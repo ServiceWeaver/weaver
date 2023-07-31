4ce07194afa0c3de3ffddd5c6ee6ae24e917123f
Michael Whittaker <mwhittaker@google.com>
2023-07-31 14:53:53 -0700

    Added blog post on metric labels.

---
+++ website/blog/metric_labels/otel/go.mod
@@ -0,0 +1,15 @@
+module otel
+
+go 1.21
+
+require (
+	go.opencensus.io v0.24.0
+	go.opentelemetry.io/otel v1.16.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+)
---
+++ website/blog/metric_labels/prometheus/main.go
@@ -0,0 +1,30 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func main() {
+	requests := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests",
+			Help: "Number of HTTP requests.",
+		},
+		[]string{"path", "status_code"},
+	)
+	requests.With(prometheus.Labels{"path": "/foo", "status_code": "404"}).Inc()
+}
