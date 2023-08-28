a9124230a67c99f58d929d23d2d22057804578fa
Michael Whittaker <mwhittaker@google.com>
2023-08-28 16:33:34 -0700

    Added basic implementation of a simulator corpus.
    See corpus.go for a description of what the simulator corpus is, how
    it's designed, why it's important, and what the next steps are. Note
    that this PR implements a very primitive corpus. There's lots to improve
    in the future.
    
    Here's an example of a corpus:
    
    ```shell
    $ tree testdata
    testdata/
    └── sim
        ├── TestCancelledSimulation
        │   └── a52f5ec5f94e674d.json
        ├── TestSimulateCorpusEntries
        │   ├── 2bfe847328319dae.json
        │   └── a52f5ec5f94e674d.json
        └── TestUnsuccessfulSimulation
            ├── 2bfe847328319dae.json
            └── a52f5ec5f94e674d.json
    
    5 directories, 5 files
    $ cat testdata/sim/TestCancelledSimulation/a52f5ec5f94e674d.json
    {
        "version": 1,
        "seed": 0,
        "num_replicas": 10,
        "num_ops": 1000
    }
    ```

---
+++ internal/sim/corpus.go
@@ -0,0 +1,149 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sim
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// # Simulator Corpus
+//
+// When the simulator runs a failed execution, it persists the failing inputs
+// to disk. The collection of saved failing inputs is called a *corpus*, and
+// each individual entry is called a *corpus entry*. When a simulator is
+// created, the first thing it does is load and re-simulate all corpus entries.
+//
+// We borrow the design of go's fuzzing library with only minor changes [1].
+// When a simulator runs as part of a test named TestFoo, it stores its corpus
+// in testdata/sim/TestFoo. Every corpus entry is a file containing a
+// JSON-encoded CorpusEntry. Filenames are derived from the hash of the
+// contents of the corpus entry. Here's an example testdata directory:
+//
+//     testdata/
+//     └── sim
+//         ├── TestCancelledSimulation
+//         │   └── a52f5ec5f94e674d.json
+//         ├── TestSimulateCorpusEntries
+//         │   ├── 2bfe847328319dae.json
+//         │   └── a52f5ec5f94e674d.json
+//         └── TestUnsuccessfulSimulation
+//             ├── 2bfe847328319dae.json
+//             └── a52f5ec5f94e674d.json
+//
+// As with go's fuzzing library, corpus entries are never garbage collected.
+// Users are responsible for manually deleting corpus entries when appropriate.
+//
+// ## Determinism
+//
+// Note that a simulation is determined by its inputs as well as the code being
+// simulated. A CorpusEntry captures the inputs to a simulation, but not the
+// code being simulated. For this reason, the way a CorpusEntry is simulated
+// will change as a user changes their code. This means that a CorpusEntry may
+// yield simulations that differ from its original execution. That is okay.
+//
+// TODO(mwhittaker): Right now, a CorpusEntry only contains a seed. This means
+// that even small changes in the user's code can lead to wildly different
+// simulations. In the future, we may want to persist the execution itself and
+// try to recreate the execution as faithfully as possible. This kind of replay
+// will also likely be needed for minimization.
+//
+// ## Visualization
+//
+// TODO(mwhittaker): Store the history of a simulation in a corpus entry so
+// failing executions can be visualized.
+//
+// ## Versioning
+//
+// The contents of a CorpusEntry may change over time. To address this, every
+// CorpusEntry contains a version. If a simulator loads a CorpusEntry with a
+// different version than what it is expecting, the CorpusEntry is ignored.
+//
+// TODO(mwhittaker): Switch to using protobufs?
+//
+// [1]: https://go.dev/security/fuzz
+
+// The current version of the CorpusEntry API.
+const version = 1
+
+// CorpusEntry is a set of failing inputs persisted for later execution.
+type CorpusEntry struct {
+	Version     int   `json:"version"`
+	Seed        int64 `json:"seed"`
+	NumReplicas int   `json:"num_replicas"`
+	NumOps      int   `json:"num_ops"`
+}
+
+// readCorpus reads all the corpus entries stored in the provided directory.
+//
+//lint:ignore U1000 This function will be used in the future.
+func readCorpus(dir string) ([]CorpusEntry, error) {
+	// This code borrows from https://cs.opensource.google/go/go/+/master:src/internal/fuzz/fuzz.go;drc=14ab998f95b53baa6e336c598b0f34e319cc9717.
+	files, err := os.ReadDir(dir)
+	if os.IsNotExist(err) {
+		return nil, nil // No corpus to read
+	} else if err != nil {
+		return nil, fmt.Errorf("read corpus %q: %w", dir, err)
+	}
+
+	var corpus []CorpusEntry
+	for _, file := range files {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".json") {
+			// The file is not a corpus entry file.
+			continue
+		}
+
+		filename := filepath.Join(dir, file.Name())
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			return nil, fmt.Errorf("read corpus entry file %q: %w", filename, err)
+		}
+		var entry CorpusEntry
+		if err := json.Unmarshal(data, &entry); err != nil {
+			return nil, fmt.Errorf("unmarshal corpus entry file %q: %w", filename, err)
+		}
+		if entry.Version != version {
+			continue
+		}
+		corpus = append(corpus, entry)
+	}
+	return corpus, nil
+
+}
+
+// writeCorpusEntry writes a corpus entry to the provided directory.
+func writeCorpusEntry(dir string, entry CorpusEntry) error {
+	// This code borrows from https://cs.opensource.google/go/go/+/master:src/internal/fuzz/fuzz.go;drc=14ab998f95b53baa6e336c598b0f34e319cc9717.
+	data, err := json.MarshalIndent(entry, "", "    ")
+	if err != nil {
+		return fmt.Errorf("marshal corpus entry: %w", err)
+	}
+	sum := fmt.Sprintf("%x", sha256.Sum256(data))[:16]
+	name := fmt.Sprintf("%s.json", sum)
+	filename := filepath.Join(dir, name)
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return err
+	}
+	// TODO(mwhittaker): Atomically write files using renames.
+	if err := os.WriteFile(filename, data, 0666); err != nil {
+		os.Remove(filename) // remove partially written file
+		return fmt.Errorf("write corpus entry file %q: %w", filename, err)
+	}
+	return nil
+}
