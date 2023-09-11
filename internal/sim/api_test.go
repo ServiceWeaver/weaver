0c8e1aa3f8ae7cf5c70c91eebab4368e07b43b6b
Michael Whittaker <mwhittaker@google.com>
2023-09-11 10:10:21 -0700

    Addressed Srdjan's feedback.
    Added a test with a workload with an Init method with a value receiver.

--- internal/sim/api_test.go
+++ internal/sim/api_test.go
@@ -82,6 +82,64 @@ func TestPassingSimulation(t *testing.T) {
 	}
 }
 
+// See TestInitByValueSimulation.
+type initByValueWorkload struct {
+	byPointerCalls int
+}
+
+func (initByValueWorkload) Init(r Registrar) error {
+	r.RegisterGenerators("ByValue")
+	r.RegisterGenerators("ByPointer")
+	return nil
+}
+
+func (initByValueWorkload) ByValue(context.Context) error {
+	return nil
+}
+
+func (*initByValueWorkload) ByPointer(context.Context) error {
+	return nil
+}
+
+func TestInitByValueSimulation(t *testing.T) {
+	// Run a simulation where the workload's Init method has a value receiver.
+	// Make sure that all methods of the workload are called.
+	s := New(t, initByValueWorkload{}, Options{})
+	r, err := s.runOne(context.Background(), options{
+		NumReplicas: 1,
+		NumOps:      1000,
+		FailureRate: 0,
+		YieldRate:   1,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r.Err != nil {
+		t.Fatal(r.Err)
+	}
+
+	byValueCalls := 0
+	byPointerCalls := 0
+	for _, event := range r.History {
+		op, ok := event.(OpStart)
+		if !ok {
+			continue
+		}
+		switch op.Name {
+		case "ByValue":
+			byValueCalls++
+		case "ByPointer":
+			byPointerCalls++
+		}
+	}
+	if byValueCalls == 0 {
+		t.Fatal("ByValue not called")
+	}
+	if byPointerCalls == 0 {
+		t.Fatal("ByPointer not called")
+	}
+}
+
 type divideByZeroWorkload struct {
 	divmod weaver.Ref[divMod]
 }
